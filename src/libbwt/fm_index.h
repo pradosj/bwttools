29302a12cfdfccb304bbc76d0640090685f0ebc7
juju <julien.prados@unige.ch>
2014-08-17 23:22:04 +0200

    add a constructor of fm index from a bwt string

--- src/libbwt/fm_index.h
+++ src/libbwt/fm_index.h
@@ -9,6 +9,7 @@
 #include <cassert>
 #include <string>
 #include <array>
+#include <istream>
 	
 
 
@@ -23,14 +24,25 @@
 template <size_t AlphabetSize>
 class fm_index {	
 	public:
-		typedef std::array<uint64_t,AlphabetSize> alpha_count64;
-
-		fm_index() {
-				//read_runs();
-				init_fm();
+		fm_index(const std::string& bwt) {
+				_bwt_size = bwt.size();
+				for(auto c:bwt) {
+						if (_runs.empty()) {
+							_runs.push_back(c);
+						} else if (!_runs.back().full() && _runs.back().value()==c) {
+								++_runs.back();
+						} else {
+								_runs.push_back(c);
+						}
+				}
+				init_fm_from_runs();
 		}
-
-
+	
+		//! \brief construct an ibject by reading the rle-encoded bwt string from a binary stream
+		fm_index(std::istream& is) {
+				_bwt_size = read_runs(is);
+				init_fm_from_runs();
+		}
 
 		//! \return total number of symbol in the bwt string
 		inline uint64_t size() const {return _bwt_size;}
@@ -38,19 +50,18 @@ class fm_index {
     inline uint64_t C(uint8_t c) const {return _C[c];}
 
     //! \return number of occurence of symbol c in bwt[0..i]
-    inline uint64_t occ(uint8_t c, uint64_t i) const {return occ(i)[c];}
-    inline alpha_count64 occ(uint64_t i) const {
+    inline uint64_t occ(uint8_t c, uint64_t i) const {
     		auto mark = previous_mark(i);
     		auto run = _runs.begin() + mark.run_index;
     		auto run_pos = std::accumulate(mark.counts.begin(),mark.counts.end(),0) - 1;
-    		while(i >= run_pos + rle_length(*run)) {
-    				run_pos += rle_length(*run);
-    				mark.counts[rle_value(*run)] += rle_length(*run);
+    		while(i >= run_pos + run->length()) {
+    				run_pos += run->length();
+    				mark.counts[run->value()] += run->length();
     				//++mark.run_index;
     				++run;
     		}
-    		mark.counts[rle_value(*run)] += (i-run_pos);
-    		return mark.counts;
+    		mark.counts[run->value()] += (i-run_pos);
+    		return mark.counts[c];
     }
 
 		//! return the suffix array interval for character b
@@ -67,12 +78,19 @@ class fm_index {
 		
 		
 		
-		
+		void print_info() {
+				std::cout << "size:" << size() << std::endl;
+				std::cout << "#run:" << _runs.size() << std::endl;
+				std::cout << "avg run size:" << (double)size() / _runs.size() << std::endl;
+				std::cout << "#marks64:" << _marks64.size() << " (" << (double) _marks64.size() * sizeof(mark64_t)/1024/1024 << "Mo)" << std::endl;
+				std::cout << "#marks16:" << _marks16.size() << " (" << (double) _marks16.size() * sizeof(mark16_t)/1024/1024 << "Mo)" << std::endl;
+		}
 		
 	private:
 			//
 			// types definitions
 			//
+			typedef std::array<uint64_t,AlphabetSize> alpha_count64;
 			typedef std::array<uint16_t,AlphabetSize> alpha_count16;
 			template <typename T1,typename T2> 
 			struct mark_t {
@@ -83,22 +101,26 @@ class fm_index {
 			};
 			typedef mark_t<uint64_t,alpha_count64> mark64_t;
 			typedef mark_t<uint16_t,alpha_count16> mark16_t;
+			struct run_t {
+					uint8_t _data;
+					run_t():_data(0) {}
+					run_t(uint8_t val):_data((val<<5) | 1) {}
+					inline uint8_t length() const {return _data & 0x1F;}
+					inline uint8_t value() const {return (_data & 0xE0)>>5;}
+					inline bool full() const {return length()>=31;}
+					inline run_t& operator++() {++_data;return *this;}
+			};
 
 
 			//
 			// attributes
 			//
 			uint64_t _bwt_size=0;
+			std::vector<run_t> _runs; // rle encoded bwt string
 			alpha_count64 _C;
-			std::vector<uint8_t> _runs; // rle encoded bwt string
 			std::vector<mark64_t> _marks64;
 			std::vector<mark16_t> _marks16;
 			
-			//
-			// RLE access methods
-			//
-			static inline uint8_t rle_length(uint8_t r) {return r & 0x1F;}
-			static inline uint8_t rle_value(uint8_t r) {return (r & 0xE0)>>5;}
 			
 
 			/*! \return the run index containg the mark preceding i and 
@@ -113,9 +135,10 @@ class fm_index {
 			}
 			
 			//! \brief iterate over _runs to initialize _C, bwt_size, _marks16 and marks64
-	    void init_fm() {
-	    		//_marks64.reserve(bwt_size>>16);
-	    		//_marks16.reserve(bwt_size>>9);
+	    void init_fm_from_runs() {
+					_marks64.reserve((size()>>16) + 1);
+		    	_marks16.reserve((size()>>9) + 1);
+		    	
 	    		std::fill(_C.begin(),_C.end(),0);
 	    		uint64_t run_index=0;
 	    		uint64_t run_pos=0;
@@ -125,8 +148,8 @@ class fm_index {
 	    						_marks16.push_back(mark16_t(run_index - _marks64.back().run_index));
 	    						std::transform(_C.begin(),_C.end(),_marks64.back().counts.begin(),_marks16.back().counts.begin(),std::minus<uint64_t>());
 	    				}
-	    				_C[rle_value(run)] += rle_length(run);
-	    				run_pos += rle_length(run);
+	    				_C[run.value()] += run.length();
+	    				run_pos += run.length();
 	    				++run_index;
 	    		}
 	
@@ -140,6 +163,25 @@ class fm_index {
 	    		}
 	    		_bwt_size = s;
 	    }
+			
+			size_t read_runs(std::istream& is) {
+					enum {BWF_NOFMI = 0,BWF_HASFMI} flag;
+			    uint16_t magic_number;
+			    size_t num_strings, num_symbols, num_runs;
+			    is.read(reinterpret_cast<char*>(&magic_number), sizeof(magic_number));
+			    if (magic_number != 0xCACA) throw std::runtime_error("BWT file is not properly formatted: the magic number provided in file header doesn't correspond to the expected one");
+			    is.read(reinterpret_cast<char*>(&num_strings), sizeof(num_strings));
+			    is.read(reinterpret_cast<char*>(&num_symbols), sizeof(num_symbols));
+			    is.read(reinterpret_cast<char*>(&num_runs), sizeof(num_runs));
+			    is.read(reinterpret_cast<char*>(&flag), sizeof(flag));
+					std::cout << "#symbols:" << num_symbols << std::endl;
+					std::cout << "#strings:" << num_strings << std::endl;			    
+			    _runs.clear();
+					_runs.resize(num_runs);
+					is.read(reinterpret_cast<char*>(&_runs[0]), num_runs*sizeof(_runs[0]));
+					return num_symbols;
+			}
+	    
 };
 
 
--- src/test.cpp
+++ src/test.cpp
@@ -1,28 +1,38 @@
 
 #include <iostream>
+#include <fstream>
+#include <algorithm>
 #include <fm_index.h>
 
 
 
 int main(int argc, char* argv[]) {
+		const std::string alphabet("$abcdr");
 		std::string bwt("ard$rcaaaabb");
-		fm_index<6> fm;
+		std::transform(bwt.begin(),bwt.end(),bwt.begin(),[&](char c){return alphabet.find(c);});
+		fm_index<6> fm(bwt);
+		
+/*
+		std::ifstream f(argv[1],std::ios::binary);
+		fm_index<5> fm(f);
+*/
+		fm.print_info();		
 		
 		std::cout << "C[]:" << std::endl;
-		for(auto c:{'$','a','b','c','d','r'}) std::cout << fm.C(c) << ' ';
+		for(auto c:{0,1,2,3,4,5}) std::cout << fm.C(c) << ' ';
 		std::cout << std::endl;
 
 		std::cout << "occ[c,i]:" << std::endl;
-		for(auto c:{'$','a','b','c','d','r'}) {
+		for(auto c:{0,1,2,3,4,5}) {
 				for(auto i=0;i<bwt.size();i++) std::cout << fm.occ(c,i);
 				std::cout << std::endl;
 		}
 		
-		interval range = fm.sa_interval('a');
+		interval range = fm.sa_interval(1);
 		std::cout << range.lower << ':' << range.upper << std::endl;
-		fm.update_sa_interval(range,'r');
+		fm.update_sa_interval(range,5);
 		std::cout << range.lower << ':' << range.upper << std::endl;
-		fm.update_sa_interval(range,'b');
+		fm.update_sa_interval(range,2);
 		std::cout << range.lower << ':' << range.upper << std::endl;
 
 		return 0;
