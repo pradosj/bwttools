e281b0adef97c8a20515a993a8af15870527f375
juju <julien.prados@unige.ch>
2014-08-18 23:18:52 +0200

    simplify test code

--- src/test.cpp
+++ src/test.cpp
@@ -3,21 +3,16 @@
 #include <fstream>
 #include <algorithm>
 #include <fm_index.h>
-#include <interval.h>
 
 
 
 int main(int argc, char* argv[]) {
 		const std::string alphabet("$abcdr");
 		std::string bwt("ard$rcaaaabb");
-		std::transform(bwt.begin(),bwt.end(),bwt.begin(),[&](char c){return alphabet.find(c);});
-		fm_index<6> fm(bwt.begin(),bwt.end());
-		
-/*
-		std::ifstream f(argv[1],std::ios::binary);
-		fm_index<5> fm(f);
-*/
-		fm.print_info(std::cout);		
+		auto encode = [&](char c){return alphabet.find(c);};
+		std::transform(bwt.begin(),bwt.end(),bwt.begin(),encode);
+		fm_index<6> fm(bwt.begin(),bwt.end());		
+		fm.print_info(std::cout);
 		
 		std::cout << "C[]:" << std::endl;
 		for(auto c:{0,1,2,3,4,5}) std::cout << fm.C(c) << ' ';
@@ -29,12 +24,18 @@ int main(int argc, char* argv[]) {
 				std::cout << std::endl;
 		}
 		
-		interval range = fm.sa_interval(1);
+		interval range = fm.sa_interval(encode('a'));
 		std::cout << range.lower << ':' << range.upper << std::endl;
-		fm.update_sa_interval(range,5);
+		fm.update_sa_interval(range,encode('r'));
 		std::cout << range.lower << ':' << range.upper << std::endl;
-		fm.update_sa_interval(range,2);
+		fm.update_sa_interval(range,encode('b'));
 		std::cout << range.lower << ':' << range.upper << std::endl;
+		
+		
+/*
+		std::ifstream f(argv[1],std::ios::binary);
+		fm_index<5> fm(f);
+*/
 
 		return 0;
 }
