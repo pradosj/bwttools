a619daf590194eec419a02af58a3558fdaa7232f
juju <julien.prados@unige.ch>
2014-08-24 22:32:49 +0200

    implement extend_rhs() but function not tested yet

--- src/libbwt/algo.h
+++ src/libbwt/algo.h
@@ -40,6 +40,30 @@ namespace bwt {
       extend_lhs(fm,low,high,low[b],high[b]);
     }
     
+    /*! \brief 1-character suffix extension of the interval [first,last) corresponding to string "S"
+     *         Extension is done with all characters of the alphabet.
+     *         Bounds of the extended intervals are set in [low[c],high[c])
+     */
+    template<size_t sz>
+    inline void extend_rhs(const fm_index<sz>& fm, typename fm_index<sz>::alpha_count64& low, typename fm_index<sz>::alpha_count64& high, uint64_t first, uint64_t last) {
+      if (first>=last) {
+				init_char_range(fm,low,high);
+      } else {
+      	// compute diff = fm.occ(last-1) - fm.occ(first-1)
+      	high = fm.occ(last-1);
+      	if (first>0) std::transform(high.begin(),high.end(),fm.occ(first-1).begin(),high.begin(),std::minus<uint64_t>());
+      	
+      	// update low and high using diff
+				uint64_t s = 0;
+      	auto i = low.begin();
+				for(auto& v:high) {
+					*i += s;
+					s += v;
+					v += *i;
+					++i;
+				}
+			}
+    }
     
 };
 
