4df5a7db09b9b6ddb671ff60d534279209ff50cd
pradosj <julien.prados@unige.ch>
2014-08-25 17:03:15 +0200

    Change the behavior of extend_lhs in case of empty input interval

--- src/libbwt/algo.h
+++ src/libbwt/algo.h
@@ -22,13 +22,14 @@ namespace bwt {
     template<size_t sz>
     inline void extend_lhs(const fm_index<sz>& fm, typename fm_index<sz>::alpha_count64& low, typename fm_index<sz>::alpha_count64& high, uint64_t first, uint64_t last) {
       if (first>=last) {
-				alpha_range(fm,low,high);
+      	std::fill(low.begin(),low.end(),first);
+      	std::fill(high.begin(),high.end(),last);
       } else {
 				//NOTE: computation of occ(.,last) might be faster using occ(.,first)
 				low = high = fm.C();
 				if (first>0) std::transform(low.begin(),low.end(),fm.occ(first-1).begin(),low.begin(),std::plus<uint64_t>());
 				std::transform(high.begin(),high.end(),fm.occ(last-1).begin(),high.begin(),std::plus<uint64_t>());
-      }	
+      }
     }
     
     /*! \brief 1-character prefix extension of the interval [low[b],high[b]) corresponding to string "S"
