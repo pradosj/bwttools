3479930a11b718d438b3529b2a820a9405b7c53f
juju <julien.prados@unige.ch>
2015-08-05 23:22:35 +0200

    several c++ simplifications

--- R/bcr.cpp
+++ R/bcr.cpp
@@ -13,7 +13,6 @@ typedef struct {
 void bcr(const uint8_t* text_begin, const uint8_t* text_end,uint8_t* bwt_begin) {
 	long i, k, n, n0;
 	uint8_t *p, *q, *B0;
-	const uint8_t *end = 0;
 	pair64_t *a;
 	int c;
 	long Tlen = std::distance(text_begin,text_end);
@@ -38,15 +37,19 @@ void bcr(const uint8_t* text_begin, const uint8_t* text_end,uint8_t* bwt_begin) 
 	
 	// core loop
 	for (i = 0, n0 = n; n0; ++i) {
-		long l, pre, ac[256], mc[256], mc2[256];
+		std::array<long,256> ac,mc,mc2;
+		mc.fill(0);mc2.fill(0);
+		
+		long pre = 0;
 		pair64_t *b[256], *aa;
-		for (c = 0; c != 256; ++c) mc[c] = mc2[c] = 0;
-		end = B0 + Blen; Blen += n0; bwt_begin -= n0;
-		for (n = k = 0, p = B0, q = bwt_begin, pre = 0; k < n0; ++k) {
+		const uint8_t *end = B0 + Blen; 
+		Blen += n0;
+		bwt_begin -= n0;
+		for (n = k = 0, p = B0, q = bwt_begin; k < n0; ++k) {
 			pair64_t *u = &a[k];
 			c = P[(u->v>>8) + 1] - 2 - i >= P[u->v>>8]? *(P[(u->v>>8) + 1] - 2 - i) : 0; // symbol to insert
 			u->v = (u->v&~0xffULL) | c;
-			for (l = 0; l != u->u - pre; ++l) // copy ($u->u - $pre - 1) symbols from B0 to B
+			for (long l = 0; l != u->u - pre; ++l) // copy ($u->u - $pre - 1) symbols from B0 to B
 				++mc[*p], *q++ = *p++; // $mc: marginal counts of all processed symbols
 			*q++ = c;
 			pre = u->u + 1; u->u = mc[c]++;
@@ -55,10 +58,12 @@ void bcr(const uint8_t* text_begin, const uint8_t* text_end,uint8_t* bwt_begin) 
 		while (p < end) ++mc[*p], *q++ = *p++; // copy the rest of $B0 to $B
 		for (c = 1, ac[0] = 0; c != 256; ++c) ac[c] = ac[c-1] + mc[c-1]; // accumulative count
 		for (k = 0; k < n; ++k) a[k].u += ac[a[k].v&0xff] + n; // compute positions for the next round
+
 		// stable counting sort ($a[k].v&0xff); also possible with an in-place non-stable radix sort, which is slower
 		aa = (pair64_t *) malloc(sizeof(pair64_t) * n);
 		for (c = 1, b[0] = aa; c != 256; ++c) b[c] = b[c-1] + mc2[c-1];
 		for (k = 0; k < n; ++k) *b[a[k].v&0xff]++ = a[k]; // this works because $a is already partially sorted
+		
 		free(a); a = aa; // $aa now becomes $a
 		B0 = bwt_begin; n0 = n;
 	}
