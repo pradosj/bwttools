5223cb25b838bb712783e9e7be9781c0456b8629
juju <julien.prados@unige.ch>
2015-08-06 07:46:32 +0200

    small changes

--- R/bcr.cpp
+++ R/bcr.cpp
@@ -1,5 +1,6 @@
 #include <Rcpp.h>
 #include <array>
+#include <algorithm>
 
 using namespace Rcpp;
 // [[Rcpp::plugins(cpp11)]]
@@ -35,15 +36,16 @@ void bcr(const uint8_t* text_begin, const uint8_t* text_end,uint8_t* bwt_begin) 
 	long n0 = n;
 	long Blen = 0;	
 	for (long i = 0; n0; ++i) {
+		long pre = 0;
+		const uint8_t *end = B0 + Blen;
+		Blen += n0;
+		bwt_begin -= n0;
+		const uint8_t *p = B0;
+		uint8_t *q = bwt_begin;
+		
 		std::array<long,256> ac,mc,mc2;
 		mc.fill(0);mc2.fill(0);
 		
-		long pre = 0;
-		const uint8_t *end = B0 + Blen; 
-		Blen += n0;
-		bwt_begin -= n0;
-		uint8_t *p = B0;
-		uint8_t *q = bwt_begin;
 		for (long k = n = 0; k < n0; ++k) {
 			pair64_t *u = &a[k];
 			int c = P[u->v+1]-2-i >= P[u->v]? *(P[u->v+1]-2-i) : 0; // symbol to insert
@@ -54,9 +56,10 @@ void bcr(const uint8_t* text_begin, const uint8_t* text_end,uint8_t* bwt_begin) 
 			pre = u->u + 1; u->u = mc[c]++;
 			if (c) a[n++] = a[k], ++mc2[c]; // $mc2: marginal counts of the current column
 		}
-		while (p < end) ++mc[*p], *q++ = *p++; // copy the rest of $B0 to $B
+		std::copy(p,end,q); // copy the rest of $B0 to $B
+		while(p < end) ++mc[*(p++)];
 		ac[0] = 0;for (int c = 1; c != 256; ++c) ac[c] = ac[c-1] + mc[c-1]; // accumulative count
-		for (long k = 0; k < n; ++k) a[k].u += ac[a[k].w] + n; // compute positions for the next round
+		for(long k = 0; k < n; ++k) a[k].u += ac[a[k].w] + n; // compute positions for the next round
 
 		// stable counting sort ($a[k].v&0xff); also possible with an in-place non-stable radix sort, which is slower
 		pair64_t *aa = (pair64_t *) malloc(sizeof(pair64_t) * n);
