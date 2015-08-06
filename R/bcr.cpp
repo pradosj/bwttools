085b04c41efb73c884f84736255b02d12a51f34c
pradosj <julien.prados@unige.ch>
2015-08-06 17:19:37 +0200

    simplify text line processing

--- R/bcr.cpp
+++ R/bcr.cpp
@@ -14,52 +14,52 @@ typedef struct {
 
 
 void bcr(const uint8_t* text_begin, const uint8_t* text_end, uint8_t* bwt_begin) {
-	
-	// split input text into lines
 	if (text_begin == text_end) return;
-	std::vector<const uint8_t*> lines;
+	
+	// find EOL, and initialize an array of iterators on the last character
+	std::vector<const uint8_t*> eol;
 	for(auto i=text_begin;i!=text_end;++i) {
-		auto line_end = std::find(i,text_end,0);
-		// if the text is not ending with eol, the last line is skipped
-		if (line_end==text_end) break;
-		lines.push_back(i);
-		i = line_end;
+		i = std::find(i,text_end,0);
+		if (i==text_end) break;
+		eol.push_back(i-1);
 	}
-	lines.push_back(text_end);
-
 	
 	// initialize
-	std::vector<pair64_t> a(lines.size()-1),aa;
+	std::vector<pair64_t> a(eol.size()),aa;
 	uint64_t k=0;for(auto &x:a) x.u = x.v = k++;
 	uint8_t *bwt_end = bwt_begin + std::distance(text_begin,text_end);
 	uint8_t *bwt0_begin = bwt_begin = bwt_end;
-
+	std::array<uint64_t,256> mc,mc2,b;
 		
 	// core loop
-	for (long i = 0; !a.empty(); ++i) {
+	while (!a.empty()) {
 		// initialize loop variables
 		uint64_t pre = 0;
 		bwt_begin -= a.size();
 		const uint8_t *p = bwt0_begin;
 		uint8_t *q = bwt_begin;
-		std::array<uint64_t,256> mc;mc.fill(0);
 		
 		// iterate over last characters of the lines ordered according to a[].v
+		mc.fill(0);
+		mc2.fill(0);
 		aa.clear();
 		for (auto &u:a) {
-			u.w = lines[u.v+1]-2-i >= lines[u.v]? *(lines[u.v+1]-2-i) : 0;
+			auto c = u.w = eol[u.v]>=text_begin?*eol[u.v]:0;
 			for (uint64_t l = 0; l != u.u - pre; ++l) {
 				++mc[*p];
 				*q++ = *p++;
 			}
-			*q++ = u.w;
+			*q++ = c;
 			pre = u.u + 1;
-			u.u = mc[u.w]++;
-			if (u.w) aa.push_back(u);
+			u.u = mc[c]++;
+			if (c) {
+				aa.push_back(u);
+				++mc2[c];
+			}
 		}
 		a.resize(aa.size());
 		
-		assert(p==q);//std::copy(p,end,q);
+		assert(p==q);
 		while(p < bwt_end) ++mc[*(p++)];
 		
 		std::array<uint64_t,256> ac;
@@ -67,11 +67,12 @@ void bcr(const uint8_t* text_begin, const uint8_t* text_end, uint8_t* bwt_begin)
 		for(auto &x:aa) x.u += ac[x.w] + aa.size(); // compute pos for next round
 
 		// stable counting sort ($a[k].w);
-		std::array<uint64_t,256> mc2,b;
-		mc2.fill(0);for(auto x:aa) ++mc2[x.w];
 		b[0] = 0;for (int c = 1; c != b.size(); ++c) b[c] = b[c-1] + mc2[c-1];
 		for(auto x:aa) a[b[x.w]++] = x; // this works because $a is already partially sorted
-		
+
+		//move EOL iterators to previous character
+		for(auto &l:eol) if (l>=text_begin && *l) --l;
+
 		bwt0_begin = bwt_begin;
 	}
 }
